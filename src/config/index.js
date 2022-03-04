3888575a29fc40400fe3d57bde63df843187616e
perezl40tp <perezl.40@nlsa.teleperformance.com>
2022-03-03 23:46:50 -0500

    mvc

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 
-const config = {
+export default {
   server: {
     port: process.env.PORT || 3000,
   },
   db: {
-    urlCnn: process.env.URI_MONGO || 'localhost',
+    host: process.env.BD_HOST || 'localhost',
+    user: process.env.BD_USER || 'root',
+    pass: process.env.BD_PASS || 'root12345',
+    database: process.env.BD_DATABASE || 'task',
   },
 }
-export default config
